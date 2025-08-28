b3abca3894f792b3c82dbbf745ffed0940021688
jehansethna <jehans7@gmail.com>
2025-08-28 00:29:12 -0400

    Testing Autoscaling

--- terraform/modules/ecs/ecs.tf
+++ terraform/modules/ecs/ecs.tf
@@ -188,7 +188,7 @@ resource "aws_ecs_service" "web_service" {
   launch_type     = "FARGATE"
   #desired_count   = var.desired_count
 
-  deployment_minimum_healthy_percent = 100
+  deployment_minimum_healthy_percent = 50
   deployment_maximum_percent = 200
 
   network_configuration {
